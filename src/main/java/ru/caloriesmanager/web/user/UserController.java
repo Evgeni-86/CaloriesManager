ebd1c3c3940f499067866a4a50907eabb67b581e
batanov evgeni <batanovevgeni@mail.ru>
2024-02-21 07:19:47 +0300

    add controller

--- src/main/java/ru/caloriesmanager/web/user/UserController.java
+++ src/main/java/ru/caloriesmanager/web/user/UserController.java
@@ -4,13 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.caloriesmanager.model.User;
+import ru.caloriesmanager.service.UserService;
 
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(value = "/")
-    public String getAll(Model model) {
+    public String index(Model model) {
         return "index";
+    }
+
+    @RequestMapping(value = "/userss")
+    public String getAllUsers(Model model) {
+        return "index2";
+    }
+
+    @RequestMapping(value = "/userss/{id}")
+    public String getUser(@PathVariable int id, Model model) {
+        User user = userService.get(id);
+        model.addAttribute("user", user);
+        return "";
     }
 }
