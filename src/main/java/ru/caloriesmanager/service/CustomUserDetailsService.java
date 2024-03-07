794270a641b224b6bdd3f378f6096b0fafe63081
evgeni batanov <batanovevgeni@mail.ru>
2024-03-07 12:55:01 +0300

    custom userdetails

--- src/main/java/ru/caloriesmanager/service/CustomUserDetailsService.java
+++ src/main/java/ru/caloriesmanager/service/CustomUserDetailsService.java
@@ -1,20 +1,14 @@
 package ru.caloriesmanager.service;
 
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.caloriesmanager.entity.User;
 import ru.caloriesmanager.repository.UserRepository;
-import ru.caloriesmanager.util.exception.NotFoundException;
 
-import java.util.Set;
-import java.util.stream.Collectors;
 
 
 @NoArgsConstructor
@@ -33,14 +27,6 @@ public class CustomUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("User not exists by Username or Email");
         }
 
-        Set<GrantedAuthority> authorities = user.getRoles().stream()
-                .map((role) -> new SimpleGrantedAuthority(role.toString()))
-                .collect(Collectors.toSet());
-
-        return new org.springframework.security.core.userdetails.User(
-                usernameOrEmail,
-                user.getPassword(),
-                authorities
-        );
+        return new CustomUserDetails(user);
     }
 }
